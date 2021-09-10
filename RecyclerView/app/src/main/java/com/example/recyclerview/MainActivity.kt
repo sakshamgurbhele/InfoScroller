1013200a082334736830618239a554201aea4c69
SakshamGurbhele <saksham9013gurbhele@gmail.com>
2021-09-11 00:01:10 +0530

    RecyclerViewApp Intial files

---
+++ RecyclerView/app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.recyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var newRecyclerview : RecyclerView
+    private lateinit var newArrayList: ArrayList<News>
+    lateinit var imageID : Array<Int>
+    lateinit var heading : Array<String>
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        heading = arrayOf(
+            "Volvo", "BMW", "Ford", "Mazda", "Volvo"
+        )
+        imageID = arrayOf(
+            R.drawable.b,
+            R.drawable.b,
+            R.drawable.b,
+            R.drawable.b,
+            R.drawable.b,
+        )
+
+
+        newRecyclerview = findViewById(R.id.Recyclerview)
+        newRecyclerview.layoutManager = LinearLayoutManager(this)
+        newRecyclerview.setHasFixedSize(true)
+
+        newArrayList = arrayListOf<News>()
+        getUserdata()
+
+    }
+
+    private fun getUserdata() {
+        for(i in heading.indices){
+            val news = News(heading[i], imageID[i])
+            newArrayList.add(news)
+        }
+        newRecyclerview.adapter = MyAdapterS(newArrayList)
+    }
+}
---
+++ RecyclerView/app/src/main/java/com/example/recyclerview/News.kt
@@ -0,0 +1,5 @@
+package com.example.recyclerview
+
+data class News(var heading: String,
+                var myimage: Int
+                )
